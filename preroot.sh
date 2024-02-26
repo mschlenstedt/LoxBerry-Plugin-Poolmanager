eb81a88de70dee8d0b696c633bda7206db413e62
Michael Schlenstedt <Michael@schlenn.net>
2024-02-26 05:39:38 +0100

    Checl if successfull

--- preroot.sh
+++ preroot.sh
@@ -10,8 +10,15 @@ ARGV5=$5 # Fifth argument is Base folder of LoxBerry
 echo "<INFO> Adding LoxBerry User to i2c group"
 usermod -a -G i2c loxberry
 
-echo "<INFO> Installing Adafruit LCD DIsplay library"
-pip3 install adafruit-circuitpython-charlcd
+echo "<INFO> Installing Adafruit LCD Display library"
+yes | python3 -m pip install adafruit-circuitpython-charlcd
+INSTALLED=$(python3 -m pip list --format=columns | grep -i "Adafruit-CharLCD" | grep -v grep | wc -l)
+if [ ${INSTALLED} -ne "0" ]; then
+	echo "<OK>  Adafruit LCD Display library installed successfully."
+else
+	echo "<FAIL>  Adafruit LCD Display library could not be installed."
+	exit 2;
+fi
 
 # Exit with Status 0
 exit 0
